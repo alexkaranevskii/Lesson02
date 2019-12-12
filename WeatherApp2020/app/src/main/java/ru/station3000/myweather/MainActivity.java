a840142b9ec09401bd66f57c562a753dd0153871
alexkaranevskii <alexkaranevskii@mail.ru>
2019-12-12 12:44:38 +0300

    Задаем цвет значениям

--- WeatherApp2020/app/src/main/java/ru/station3000/myweather/MainActivity.java
+++ WeatherApp2020/app/src/main/java/ru/station3000/myweather/MainActivity.java
@@ -1,8 +1,9 @@
 package ru.station3000.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI(){
+
+        TextView tetextViewTime = findViewById(R.id.textViewTime);
+        Resources resoursces = getResources();
+        tetextViewTime.setText("11.11");
+        tetextViewTime.setTextColor(resoursces.getColor(R.color.colorPrimary));
+
+
+        TextView textViewTempO = findViewById(R.id.textViewTempO);
+        Resources resoursces2 = getResources();
+        textViewTempO.setText("o");
+        tetextViewTime.setTextColor(resoursces.getColor(R.color.colorPrimaryDark));
     }
 }
