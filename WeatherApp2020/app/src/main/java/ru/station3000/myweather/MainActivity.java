5b1221260a07fb2a3033387804eaad0612b5f98e
alexkaranevskii <alexkaranevskii@mail.ru>
2019-12-18 08:35:21 +0300

    Куча мала Сделана заготовка приложения с несколькими Layout

--- WeatherApp2020/app/src/main/java/ru/station3000/myweather/MainActivity.java
+++ WeatherApp2020/app/src/main/java/ru/station3000/myweather/MainActivity.java
@@ -1,21 +1,30 @@
 package ru.station3000.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button2;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUI();
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
     }
 
-    private void setUI(){
-
+    private void setUI() {
         TextView tetextViewTime = findViewById(R.id.textViewTime);
         Resources resoursces = getResources();
         tetextViewTime.setText("11.11");
@@ -23,8 +32,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         TextView textViewTempO = findViewById(R.id.textViewTempO);
-        Resources resoursces2 = getResources();
         textViewTempO.setText("o");
         tetextViewTime.setTextColor(resoursces.getColor(R.color.colorPrimaryDark));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case  R.id.button2:
+                Intent intent = new Intent(this, Activity2.class);
+            default:
+                break;
+        }
     }
 }
