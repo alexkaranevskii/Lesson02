dd0f354aead24eaa390cb7c59bdb8c5ab82f62d0
alexkaranevskii <alexkaranevskii@mail.ru>
2019-12-19 12:32:18 +0300

    Стал проект на проверку

--- Lesson2_2/app/src/main/java/ru/station3000/myapplicationw/MainActivity.java
+++ Lesson2_2/app/src/main/java/ru/station3000/myapplicationw/MainActivity.java
@@ -3,10 +3,13 @@ package ru.station3000.myapplicationw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity{
     private LinearLayout load_layout;
     private LinearLayout working_layout;
     private LinearLayout city_layout;
+    private LinearLayout search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +33,33 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
         ListView listView = findViewById(R.id.listView);
+        EditText editText = (EditText) findViewById(R.id.editText);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
         android.R.layout.simple_list_item_1, city);
         listView.setAdapter(adapter);
 
+
         working_layout = findViewById(R.id.working_layout);
         city_layout = findViewById(R.id.city_layout);
         load_layout = findViewById(R.id.load_layout);
+        search = findViewById(R.id.search);
 
         working_layout.setVisibility(View.GONE);
         city_layout.setVisibility(View.GONE);
+        search.setVisibility(View.GONE);
 
-        // Возврат к рабочей страничке
-        //  Включаем видимость working_layout
+        editText.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(s);
+            }
+        });
+
+          // Возврат к рабочей страничке
+          //  Включаем видимость working_layout
         Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,8 +67,12 @@ public class MainActivity extends AppCompatActivity{
                 working_layout.setVisibility(View.VISIBLE);
                 city_layout.setVisibility(View.GONE);
                 load_layout.setVisibility(View.GONE);
+                search.setVisibility(View.GONE);
             }
         });
+
+
+
 
 
         // Запуск погодного приложения
@@ -63,6 +84,7 @@ public class MainActivity extends AppCompatActivity{
                 working_layout.setVisibility(View.VISIBLE);
                 city_layout.setVisibility(View.GONE);
                 load_layout.setVisibility(View.GONE);
+                search.setVisibility(View.GONE);
             }
         });
 
@@ -74,6 +96,7 @@ public class MainActivity extends AppCompatActivity{
                 working_layout.setVisibility(View.GONE);
                 city_layout.setVisibility(View.VISIBLE);
                 load_layout.setVisibility(View.GONE);
+                search.setVisibility(View.GONE);
             }
         });
 
